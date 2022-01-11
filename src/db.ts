07b3dac6e39bc25022c60dce5c1ad39a0fb80112
SalazarF92 <72422785+SalazarF92@users.noreply.github.com>
2022-01-11 20:46:42 -0300

    fixed DB

--- src/db.ts
+++ src/db.ts
@@ -1,14 +1,15 @@
 import { createConnection } from "typeorm";
 import { Digimon } from "./entity/digimon";
+import { POSTGRES_HOST, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DATABASE } from "./settings";
 
 export const connection = createConnection({
   name: "default",
   type: "postgres",
-  host: "localhost",
+  host: POSTGRES_HOST,
   port: 5432,
-  username: "digimon",
-  password: "digimon",
-  database: "digimon",
+  username: POSTGRES_USER,
+  password: POSTGRES_PASSWORD,
+  database: POSTGRES_DATABASE,
   entities: [Digimon],
   synchronize: true,
   logging: false,
