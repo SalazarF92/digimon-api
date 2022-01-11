2c94b24093ce44601d4032a5397f276f7d0e8f17
SalazarF92 <72422785+SalazarF92@users.noreply.github.com>
2022-01-11 17:02:49 -0300

    deletou tudo!

---
+++ src/settings.ts
@@ -0,0 +1,6 @@
+export const POSTGRES_HOST = process.env.POSTGRES_HOST;
+export const POSTGRES_USER = process.env.POSTGRES_USER;
+export const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD;
+export const POSTGRES_DATABASE = process.env.POSTGRES_DATABASE;
+
+
